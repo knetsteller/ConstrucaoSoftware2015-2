e3c9c6f9f6a60ff3133e38094457c45b25beccf0
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:42:30 -0200

    Refatorado teste do Algoritmo31.

--- Algoritmo31/src/test/java/br/ufg/inf/es/construcao/algoritmo31/PolinomioTest.java
+++ Algoritmo31/src/test/java/br/ufg/inf/es/construcao/algoritmo31/PolinomioTest.java
@@ -1,39 +1,17 @@
 package br.ufg.inf.es.construcao.algoritmo31;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Netsteller
- */
 public class PolinomioTest {
     Polinomio p;
     int[] array = {1, 2, 3};
     int[] arrayVazio = {};
 
-    public PolinomioTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         p = new Polinomio();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
