cfc5a562e97120dcacb75c7d7a6792d0610de5c8
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:25:24 -0200

    Refatorado teste do Algoritmo12.

--- Algoritmo12/src/test/java/br/ufg/inf/es/construcao/algoritmo12/MdcTest.java
+++ Algoritmo12/src/test/java/br/ufg/inf/es/construcao/algoritmo12/MdcTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo12;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MdcTest {
     Mdc mdc;
 
-    public MdcTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         mdc = new Mdc();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
@@ -36,17 +18,17 @@ public class MdcTest {
     }
     
     @Test(expected = IllegalArgumentException.class)
-    public void testCalculaMaximoDivisorAMenorQueBLancaExcecao() {
+    public void testCalculaMaximoDivisorAMenorQueB() {
         mdc.calculaMaximoDivisorComum(2, 4);
     }
     
     @Test(expected = IllegalArgumentException.class)
-    public void testCalculaMaximoDivisorBIgualZeroLancaExcecao() {
+    public void testCalculaMaximoDivisorBIgualZero() {
         mdc.calculaMaximoDivisorComum(2, 0);
     }
     
     @Test(expected = IllegalArgumentException.class)
-    public void testCalculaMaximoDivisorAInvalidoBInvalidoLancaExcecao() {
+    public void testCalculaMaximoDivisorAInvalidoBInvalido() {
         mdc.calculaMaximoDivisorComum(-2, -1);
     }
 }
