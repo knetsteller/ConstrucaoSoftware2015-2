735549fd7ffe60b9cc9d03de1f5b894eed5964d9
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:34:25 -0200

    Refatorado teste do Algoritmo24.

--- Algoritmo24/src/test/java/br/ufg/inf/es/construcao/algoritmo24/DivideSomasTest.java
+++ Algoritmo24/src/test/java/br/ufg/inf/es/construcao/algoritmo24/DivideSomasTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo24;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DivideSomasTest {
     DivideSomas divideSomas;
 
-    public DivideSomasTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         divideSomas = new DivideSomas();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
