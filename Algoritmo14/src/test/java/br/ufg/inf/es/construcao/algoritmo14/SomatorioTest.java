2017b53a6e63de3a3a0863dab19f404a358a01ec
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:28:29 -0200

    Refatorado teste do Algoritmo14.

--- Algoritmo14/src/test/java/br/ufg/inf/es/construcao/algoritmo14/SomatorioTest.java
+++ Algoritmo14/src/test/java/br/ufg/inf/es/construcao/algoritmo14/SomatorioTest.java
@@ -1,26 +1,12 @@
 package br.ufg.inf.es.construcao.algoritmo14;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SomatorioTest {
     
     Somatorio somatorio;
-
-    public SomatorioTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
 
     @Before
     public void setUp() {
