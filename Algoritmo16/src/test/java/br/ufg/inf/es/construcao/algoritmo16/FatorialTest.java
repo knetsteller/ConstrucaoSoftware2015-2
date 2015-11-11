3437f2e07b693acef699746af6bf4817942f2c53
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:29:52 -0200

    Refatorado teste do Algoritmo16.

--- Algoritmo16/src/test/java/br/ufg/inf/es/construcao/algoritmo16/FatorialTest.java
+++ Algoritmo16/src/test/java/br/ufg/inf/es/construcao/algoritmo16/FatorialTest.java
@@ -1,35 +1,17 @@
 package br.ufg.inf.es.construcao.algoritmo16;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FatorialTest {    
     Fatorial fatorial;
     
-    public FatorialTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         fatorial = new Fatorial();
     }
 
-    @After
-    public void tearDown() {
-    }
-    
     @Test
     public void testCalculaFatorialNIgualAUm() {
         assertEquals(1, fatorial.calculaFatorial(1));
