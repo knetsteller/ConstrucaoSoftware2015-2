f590c016ac3bb85cdfa277bd547b014a636da4a7
Netsteller <kelvesoad@hotmail.com>
2015-11-11 11:29:43 -0200

    Refatorado teste do Algoritmo42.

--- Algoritmo42/src/test/java/br/ufg/inf/es/construcao/algoritmo42/SomaNaturaisTest.java
+++ Algoritmo42/src/test/java/br/ufg/inf/es/construcao/algoritmo42/SomaNaturaisTest.java
@@ -1,35 +1,17 @@
 package br.ufg.inf.es.construcao.algoritmo42;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SomaNaturaisTest {
     SomaNaturais somaNaturais;
 
-    public SomaNaturaisTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         somaNaturais = new SomaNaturais();
     }
 
-    @After
-    public void tearDown() {
-    }
-    
     @Test
     public void testSomaNaturaisNIgualUm() {
         assertEquals(1, somaNaturais.somaNaturais(1));
