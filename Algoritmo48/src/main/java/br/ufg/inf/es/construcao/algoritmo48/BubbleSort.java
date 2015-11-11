e56ff8c528cd471238a2637bd183bc0d4aafd1db
Netsteller <kelvesoad@hotmail.com>
2015-11-11 12:49:14 -0200

    Resolvido Algoritmo48.

--- Algoritmo48/src/main/java/br/ufg/inf/es/construcao/algoritmo48/BubbleSort.java
+++ Algoritmo48/src/main/java/br/ufg/inf/es/construcao/algoritmo48/BubbleSort.java
@@ -4,25 +4,24 @@ package br.ufg.inf.es.construcao.algoritmo48;
  * Classe contendo o método de ordenação bubblesort.
  */
 public class BubbleSort {
-    
+    static int[] array;
     /**
      * Realiza ordenação dos elementos por bubblesort.
-     * @param array Array a ser ordenado.
+     * @param a Array a ser ordenado.
      * @param n
      * @return Array com os elementos ordenados de forma crescente.
      */
-    public int[] ordenaArrayPorBubbleSort(int[] a, int n) {
+    public static int[] ordenaArrayPorBubbleSort(int[] a, int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n dever maior ou igual a 0.");
         }
         
-        int[] array = new int[n];
         array = a;
-        int p = n;        
+        int p = a.length - 1;        
         while (p >= 1) {            
-            int indice = 1;
+            int indice = 0;
             while (indice < p) {                
-                if (array[indice] < array[indice + 1]) {
+                if (array[indice] > array[indice + 1]) {
                     int elementoTemporario = array[indice];
                     array[indice] = array[indice + 1];
                     array[indice + 1] = elementoTemporario;
--- Algoritmo48/src/test/java/br/ufg/inf/es/construcao/algoritmo48/BubbleSortTest.java
+++ Algoritmo48/src/test/java/br/ufg/inf/es/construcao/algoritmo48/BubbleSortTest.java
@@ -1,36 +1,18 @@
 package br.ufg.inf.es.construcao.algoritmo48;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BubbleSortTest {
     BubbleSort bubbleSort;
-    int[] arrayOrdenado = {1, 3, 4, 6, 8, 9};
-    int[] array = {6, 9, 3, 8, 1, 4};
-    int[] a = new int[6];
-
-    public BubbleSortTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    static int[] arrayOrdenado = {1, 3, 4, 6, 8, 9};
+    static int[] array = {6, 9, 3, 8, 1, 4};
+    static int[] a = new int[6];
 
     @Before
     public void setUp() {
         bubbleSort = new BubbleSort();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
