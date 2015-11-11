0f3d96fe78ec2027012ef1cbb94d016baca75e75
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:27:14 -0200

    Refatorado teste do Algoritmo13.

--- Algoritmo13/src/test/java/br/ufg/inf/es/construcao/algoritmo13/NumeroHarmonicoTest.java
+++ Algoritmo13/src/test/java/br/ufg/inf/es/construcao/algoritmo13/NumeroHarmonicoTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo13;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class NumeroHarmonicoTest {
     NumeroHarmonico numeroHarmonico;
 
-    public NumeroHarmonicoTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         numeroHarmonico = new NumeroHarmonico();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
