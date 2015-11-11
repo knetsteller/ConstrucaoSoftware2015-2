37e9f6474ed55bc6a3295bccace5783adf5ce066
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:32:43 -0200

    Refatorado teste do Algoritmo21.

--- Algoritmo21/src/test/java/br/ufg/inf/es/construcao/algoritmo21/QuadradoPerfeitoTest.java
+++ Algoritmo21/src/test/java/br/ufg/inf/es/construcao/algoritmo21/QuadradoPerfeitoTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo21;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QuadradoPerfeitoTest {
     QuadradoPerfeito quadradoPerfeito;
 
-    public QuadradoPerfeitoTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         quadradoPerfeito = new QuadradoPerfeito();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
