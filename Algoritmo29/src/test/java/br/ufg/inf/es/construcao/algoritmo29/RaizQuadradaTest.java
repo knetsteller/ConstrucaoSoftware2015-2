063d212c44ed961640e5991083632897efa5a4b9
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:40:55 -0200

    Refatorado teste do Algoritmo29.

--- Algoritmo29/src/test/java/br/ufg/inf/es/construcao/algoritmo29/RaizQuadradaTest.java
+++ Algoritmo29/src/test/java/br/ufg/inf/es/construcao/algoritmo29/RaizQuadradaTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo29;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RaizQuadradaTest {
     RaizQuadrada raizQuadrada;
 
-    public RaizQuadradaTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         raizQuadrada = new RaizQuadrada();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
