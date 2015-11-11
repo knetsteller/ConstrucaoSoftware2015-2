219e15cef166ae006df34a056943906ebfc854c4
Netsteller <kelvesoad@hotmail.com>
2015-11-11 10:33:33 -0200

    Refatorado teste do Algoritmo22.

--- Algoritmo22/src/test/java/br/ufg/inf/es/construcao/algoritmo22/AmigosTest.java
+++ Algoritmo22/src/test/java/br/ufg/inf/es/construcao/algoritmo22/AmigosTest.java
@@ -1,33 +1,15 @@
 package br.ufg.inf.es.construcao.algoritmo22;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AmigosTest {
     Amigos amigos;
 
-    public AmigosTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         amigos = new Amigos();
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
